76f700962a0c1ca7175a9eee21d242b92b6d1474
Nicholas Long <Nicholas.J.Long2018@gmail.com>
2021-04-04 10:35:04 -0700

    working on errors

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 $(".saveBtn").on("click", function () {
@@ -14,7 +13,7 @@ $("#am9 .form-control").val(localStorage.getItem("am9"));
 $("#am10 .form-control").val(localStorage.getItem("am10"));
 $("#am11 .form-control").val(localStorage.getItem("am11"));
 $("#pm12 .form-control").val(localStorage.getItem("pm12"));
-$("#pm1.form-control").val(localStorage.getItem("pm1"));
+$("#pm1 .form-control").val(localStorage.getItem("pm1"));
 $("#pm2 .form-control").val(localStorage.getItem("pm2"));
 $("#pm3 .form-control").val(localStorage.getItem("pm3"));
 $("#pm4 .form-control").val(localStorage.getItem("pm4"));
@@ -25,20 +24,20 @@ function hourTracker() {
 
 
     $('.time-block').each(function () {
-        var timeSlot = parseInt($(this).attr('id').split('hour')[1]);
-        console.log(timeSlot, currentHour)
+        var blockHour = parseInt($(this).attr('id').split('hour')[1]);
+        console.log(blockHour, currentHour)
 
-        if (timeSlot < currentHour) {
+        if (blockHour < currentHour) {
             $(this).addClass('past');
             $(this).removeClass('future');
             $(this).removeClass('present');
         }
-        else if (timeSlot > currentHour) {
+        else if (blockHour > currentHour) {
             $(this).addClass('future');
             $(this).removeClass('past');
             $(this).removeClass('present');
         }
-        else (timeSlot === currentHour) {
+        else {
             $(this).addClass('present');
             $(this).removeClass('past');
             $(this).removeClass('future');
@@ -46,4 +45,3 @@ function hourTracker() {
     })
 }
 hourTracker();
-})
